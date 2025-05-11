a092424bf8ad4aeeea4d8b0fc1c7ea5697e862d0
edmorua <eduardo.r.morua@gmail.com>
2025-05-11 14:31:40 -0600

    adding after afterthrowing and afterreturning aop annotations

--- src/main/java/com/apsus/learn_spring_aop/aspects/LoggingAspect.java
+++ src/main/java/com/apsus/learn_spring_aop/aspects/LoggingAspect.java
@@ -4,10 +4,7 @@ package com.apsus.learn_spring_aop.aspects;
 //AOP
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -18,14 +15,32 @@ public class LoggingAspect {
 
 	private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 	//Pointcut - when? expression that identifies method calls to be intercepted
-	@Before("execution(* com.apsus.learn_spring_aop.data.*.*(..))")
+	@Before("execution(* com.apsus.learn_spring_aop.*.*.*(..))")
 	public void logMethodCall(JoinPoint joinPoint) {
 		// Logic - What?
-		logger.info("Before method is called - {}", joinPoint.getSignature().getName());
+		logger.info("Before method is called - {} - args - {}", joinPoint, joinPoint.getArgs());
 	}
 
-	@After("execution(* com.apsus.learn_spring_aop.business.*.*(..))")
-	public void logMethodReturn(JoinPoint joinPoint) {
-		logger.info("After method is called - {}", joinPoint.getSignature().getName());
+	@After("execution(* com.apsus.learn_spring_aop.*.*.*(..))")
+	public void logMethodAfter(JoinPoint joinPoint) {
+		logger.info("After method is called - {}  has executed", joinPoint);
+	}
+
+
+	@AfterThrowing(
+		pointcut = "execution(* com.apsus.learn_spring_aop.*.*.*(..))",
+		throwing = "exception"
+	)
+	public void logMethodAfterThrowing(JoinPoint joinPoint, Exception exception) {
+		logger.info("AfterThrowing method is called - {} has thrown an exception {}", joinPoint, exception.getMessage());
+	}
+
+
+	@AfterReturning(
+		pointcut = "execution(* com.apsus.learn_spring_aop.*.*.*(..))",
+		returning = "resultValue"
+	)
+	public void logMethodAfterReturning(JoinPoint joinPoint, Object resultValue) {
+		logger.info("AfterReturning method is called - {} has returned {}", joinPoint, resultValue);
 	}
 }
